01680653fa5d76322f0d10c13216ed5ee12a9649
VadimUA <puet12vadim@gmail.com>
2018-10-26 02:41:32 +0300

    Clean user service

--- app/services/user.service.js
+++ app/services/user.service.js
@@ -5,47 +5,38 @@
         .module('service.userService', [])
         .service('userService', userService);
 
-    userService.$inject = ['http', 'url', '$localStorage', '$state', 'toastr'];
+    userService.$inject = ['http', 'url', '$localStorage'];
 
-    function userService(http, url, $localStorage, $state, toastr) {
+    function userService(http, url, $localStorage) {
 
         let model = {};
         model.getConsultantList = getConsultantList;
         model.setConsultantList = setConsultantList;
         model.getKidsList = getKidsList;
-
         model.getParents = getParents;
-
 
         return model;
 
 
         function getConsultantList() {
             return http.get(url.consultant.list).then(function (res) {
-                if (res.status === 'success') {
-                    return res.data;
-                } else {
-                    return []
-                }
+                return res.status === 'success' ? res.data : [];
             });
         }
+
         function setConsultantList(user) {
             $localStorage.user = user;
         }
+
         function getKidsList() {
             return http.get(url.kids.list).then(function (res) {
-                if (res.status === 'success') {
-                    return res.data;
-                } else {
-                    return []
-                }
+                return res.status === 'success' ? res.data : [];
             })
         }
 
         function getParents(data) {
             return http.post(url.parents.list, data);
         }
-
 
     }
 })();
